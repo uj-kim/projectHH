4374dd18b19bb6447e14c0ea726ed59f3826a20c
uj-kim <uj-kim@users.noreply.github.com>
2025-02-11 19:16:43 +0900

    fix: 로그인 전 session 오류 해결

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,18 +1,20 @@
 // src/hooks/useAuth.ts
-
 import { useQuery } from '@tanstack/react-query';
 import { supabase } from '@/lib/supabaseClient';
 import { User } from '@supabase/supabase-js';
 
 const fetchUser = async (): Promise<User | null> => {
-    const { data, error } = await supabase.auth.getUser();
-    if (data.user == null) {
-        return data.user;
+    // 먼저 세션 확인
+    const { data: sessionData, error: sessionError } = await supabase.auth.getSession();
+    if (sessionError) {
+        throw new Error(sessionError.message);
     }
-    else if (error) {
-        throw new Error(error.message);
+    if (!sessionData?.session) {
+        // 로그인하지 않은 상태이면 null 반환
+        return null;
     }
-    return data.user;
+    // 세션이 존재하면 세션에 포함된 사용자 정보를 반환
+    return sessionData.session.user;
 };
 
 export const useAuth = () => {
