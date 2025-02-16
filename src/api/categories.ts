588ffb4651e96ae8aa3b7ff0987c0587ea91ab43
uj-kim <uj-kim@users.noreply.github.com>
2025-02-16 20:46:23 +0900

    Revert "resolve: 카테고리 이미지 최적화(규격지정, 퀄리티 조정) 및 성능 향상"
    This reverts commit dd4641463f9d3bd43c528b90fb178ea9c338c0dd.

--- src/api/categories.ts
+++ src/api/categories.ts
@@ -4,38 +4,38 @@ import { supabase } from "@/lib/supabaseClient";
 import { getImgixUrl } from "@/lib/imgix";
 
 export const getAllCategories = async () => {
-  const { data, error } = await supabase.from('categories').select('*');
+    const {data, error} = await supabase.from('categories').select('*');
 
-  if (error) {
-    throw new Error(error.message);
-  }
+    if (error) {
+        throw new Error(error.message);
+    }
 
-  // 각 카테고리의 이미지 URL을 Imgix URL로 변환
-  const optimizedData = data.map((category) => ({
-    ...category,
-    category_image_url: category.category_image_url
-      ? getImgixUrl(category.category_image_url)
-      : null,
-  }));
+    // 각 카테고리의 이미지 URL을 Imgix URL로 변환
+    const optimizedData = data.map((category) => ({
+        ...category,
+        category_image_url: category.category_image_url
+        ? getImgixUrl(category.category_image_url)
+        : null,
+    }));
 
-  return optimizedData;
-};
+    return optimizedData;
+}
 
-// 카테고리 페이지 - 해당 카테고리 이름 표시
+//카테고리 페이지_ 해당 카테고리 이름 표시
 export const getCategoryById = async (category_id: string) => {
-  const { data, error } = await supabase
+    const {data, error} = await supabase
     .from('categories')
     .select('*')
     .eq('category_id', category_id)
-    .single();
+    .single()
 
-  if (error) {
-    throw new Error(error.message);
-  }
-  
-  if (data.category_image_url) {
-    data.category_image_url = getImgixUrl(data.category_image_url);
-  }
+    if(error) {
+        throw new Error(error.message);
+    }
+    
+    if (data.category_image_url) {
+        data.category_image_url = getImgixUrl(data.category_image_url);
+      }
 
-  return data;
-};
+    return data;
+}
