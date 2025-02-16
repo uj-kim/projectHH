908805e9bd6845b61bc26bedf305b621c7589c04
uj-kim <uj-kim@users.noreply.github.com>
2025-02-16 23:16:36 +0900

    resolves: 성능 최적화 - 카테고리 이미지 규격지정 및 퀄리티 조정, 페이지 렌더링 시 레이아웃 변경 지점 해결(index.css- body)

--- src/lib/imgix.ts
+++ src/lib/imgix.ts
@@ -1,11 +1,15 @@
 export const getImgixUrl = (
-    originalUrl: string,
-    options: { fm?: string; q?:string } = { fm: 'webp', q:'70' }
-  ): string => {
-    const imgixDomain = 'https://hanghae-805378545.imgix.net';
-    // Supabase의 public URL 경로에서 실제 파일 경로만 추출 (예: '/your-bucket/path/to/image.jpg')
-    const urlPath = new URL(originalUrl).pathname.split('/object/public/category-images/')[1];
-    const params = new URLSearchParams(options).toString();
-    return `${imgixDomain}/${urlPath}?${params}`;
-  };
-  
+  originalUrl: string,
+  options: { fm?: string; q?: string; w?: string; h?: string; fit?: string } = {
+    fm: 'webp',
+    q: '90',
+    w: '192',  // 실제 요청 크기를 늘림
+    h: '192',
+    fit: 'max',
+  }
+): string => {
+  const imgixDomain = 'https://hanghae-805378545.imgix.net';
+  const urlPath = new URL(originalUrl).pathname.split('/object/public/category-images/')[1];
+  const params = new URLSearchParams(options).toString();
+  return `${imgixDomain}/${urlPath}?${params}`;
+};
