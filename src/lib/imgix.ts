dd4641463f9d3bd43c528b90fb178ea9c338c0dd
uj-kim <uj-kim@users.noreply.github.com>
2025-02-16 20:29:34 +0900

    resolve: 카테고리 이미지 최적화(규격지정, 퀄리티 조정) 및 성능 향상

--- src/lib/imgix.ts
+++ src/lib/imgix.ts
@@ -1,11 +1,16 @@
 export const getImgixUrl = (
-    originalUrl: string,
-    options: { fm?: string; q?:string } = { fm: 'webp', q:'70' }
-  ): string => {
-    const imgixDomain = 'https://hanghae-805378545.imgix.net';
-    // Supabase의 public URL 경로에서 실제 파일 경로만 추출 (예: '/your-bucket/path/to/image.jpg')
-    const urlPath = new URL(originalUrl).pathname.split('/object/public/category-images/')[1];
-    const params = new URLSearchParams(options).toString();
-    return `${imgixDomain}/${urlPath}?${params}`;
-  };
-  
+  originalUrl: string,
+  options: { fm?: string; q?: string; w?: string; h?: string; fit?: string } = {
+    fm: 'webp',
+    q: '75',
+    w: '192',
+    h: '192',
+    fit: 'max',
+  }
+): string => {
+  const imgixDomain = 'https://hanghae-805378545.imgix.net';
+  // Supabase의 public URL 경로에서 '/object/public/category-images/' 이후의 파일 경로만 추출
+  const urlPath = new URL(originalUrl).pathname.split('/object/public/category-images/')[1];
+  const params = new URLSearchParams(options).toString();
+  return `${imgixDomain}/${urlPath}?${params}`;
+};
