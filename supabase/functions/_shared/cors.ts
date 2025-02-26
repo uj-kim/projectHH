43ccc5de97b395b11fd0aaf8c1af673f6329f5d6
uj-kim <uj-kim@users.noreply.github.com>
2025-02-26 16:06:50 +0900

    fix: supabase edge function cors 헤더 설정

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -1,5 +1,5 @@
 export const corsHeaders = {
-  "Access-Control-Allow-Origin": "http://localhost:4173",
+  "Access-Control-Allow-Origin": "https://project-52mb8vo4d-ujkims-projects.vercel.app/",
   "Access-Control-Allow-Headers":
     "x-csrf-token, x-requested-with, accept, accept-version, content-length, content-md5, date, x-api-version, authorization, x-client-info, apikey, content-type",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
