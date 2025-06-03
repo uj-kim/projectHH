39fc3fc058a1f9194a09d6dafc1e2bf581b4245c
uj-kim <ninakyj@gmail.com>
2025-06-03 15:24:16 +0900

    fix: vercel에서 메인페이지 외 초기접속시 404에러 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,21 @@
-import { defineConfig, type PluginOption } from 'vite';
-import react from '@vitejs/plugin-react';
-import { visualizer } from 'rollup-plugin-visualizer';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { defineConfig, type PluginOption } from "vite";
+import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
+import path from "path";
+import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/",
     plugins: [
         react(),
-        visualizer({open: false}) as PluginOption,
+        visualizer({ open: false }) as PluginOption,
     ],
     resolve: {
         alias: {
-            '@': path.resolve(__dirname, 'src'),
+            "@": path.resolve(__dirname, "src"),
         },
     },
 });
